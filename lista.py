69de980e62cf07e079a034e5813a1e6a9dfb60d6
Isabelascolla <102077830+Isabelascolla@users.noreply.github.com>
2022-04-11 16:09:12 -0300

    Add files via upload

---
+++ lista.py
@@ -0,0 +1,16 @@
+
+
+from sre_constants import OP_IGNORE
+
+
+grampeador=[1,2,3,5,7,11]
+
+print(grampeador)
+print (grampeador[0])
+
+grampeador.remove[1]
+print(grampeador)
+
+
+for x in grampeador:
+    print("oi")
